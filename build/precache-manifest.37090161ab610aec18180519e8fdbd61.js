c2ccd33b1a366f52ae6f1565621742c8dd576a7a
Suman Jha <suman.jha@fiserv.com>
2020-09-06 16:35:49 -0700

    adding production build

---
+++ build/precache-manifest.37090161ab610aec18180519e8fdbd61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796a1cce7899330e10ea7e7b521473c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a345abb18e73b84d8b5b",
+    "url": "/static/css/main.e676c53c.chunk.css"
+  },
+  {
+    "revision": "d2d6540d63caf149956e",
+    "url": "/static/js/2.ea143a10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ea143a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a345abb18e73b84d8b5b",
+    "url": "/static/js/main.02776754.chunk.js"
+  },
+  {
+    "revision": "43879a7175c3e740616a",
+    "url": "/static/js/runtime-main.686b7e5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
